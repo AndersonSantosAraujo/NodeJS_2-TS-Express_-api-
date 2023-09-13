0b2e385df2490ef3205ce35d322c4ee0cf8bb42c
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-09-13 15:07:06 -0300

    feat: yup config and translations (#10)

--- src/server/controllers/cities/create.controller.ts
+++ src/server/controllers/cities/create.controller.ts
@@ -12,14 +12,20 @@ export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
 
   // Yup Validation ->
   try {
-    validateData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
+    validateData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
     });
   }
   // <- Yup Validation
