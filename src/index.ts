ed74c7e20e26a118c2afbc2431c8fdcd7b1d56c4
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-09-12 23:49:25 -0300

    feat: tsc config (#06)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { server } from "./server/server";
 
-server.listen(3333, () => {
-  console.log("Server running on http://localhost:3333/");
+const port = process.env.PORT || 3333;
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
