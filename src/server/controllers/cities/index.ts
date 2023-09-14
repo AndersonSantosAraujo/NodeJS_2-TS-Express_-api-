ca24dc13481472a0583a158125ecec9c0595b487
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-09-14 19:16:28 -0300

    feat: tests config (#15)

--- src/server/controllers/cities/index.ts
+++ src/server/controllers/cities/index.ts
@@ -1,8 +1,8 @@
 import * as create from "./create.controller";
-import * as getAll from "./get-all.controller";
-import * as getById from "./get-by-id.controller";
-import * as deleteById from "./detele-by-id.controller";
-import * as updateById from "./update-by-id.controller";
+import * as getAll from "./getAll.controller";
+import * as getById from "./getById.controller";
+import * as deleteById from "./deleteById.controller";
+import * as updateById from "./updateById.controller";
 
 export const citiesController = {
   ...create,
