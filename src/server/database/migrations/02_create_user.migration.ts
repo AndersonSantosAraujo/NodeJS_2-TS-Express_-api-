e0f7b36eba8032c8b6a66869f71c959f9b981483
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-09-23 00:28:45 -0300

    fix: checkLength on users provider (#34.1)

--- src/server/database/migrations/02_create_user.migration.ts
+++ src/server/database/migrations/02_create_user.migration.ts
@@ -5,9 +5,9 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema
     .createTable(TableNames.user, (table) => {
       table.bigIncrements("id").primary().index();
-      table.string("name").notNullable().checkLength(">", 3);
-      table.string("email").index().unique().notNullable().checkLength(">", 5);
-      table.string("password").notNullable().checkLength(">", 6);
+      table.string("name").notNullable().checkLength(">=", 3);
+      table.string("email").index().unique().notNullable().checkLength(">=", 5);
+      table.string("password").notNullable().checkLength(">=", 6);
 
       table.comment("Tabela usada para armazenar usuários no sistema.");
     })
